46d1142cba33812d4893b7ede4ce7a594be9adfc
AhranD <158118722+AhranD@users.noreply.github.com>
2024-12-03 03:25:48 +0000

    hashed library

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -1,8 +1,18 @@
+import hashlib
+from hashlib import blake2b
+h = blake2b()
+
+
 def readFile(inFile):
     file = open(inFile, "r")
     words = file.read()
     wordsList = words.split("\n")
+    for word in wordsList:
+        h.update(b'word')
     print(wordsList)
     
 if __name__ == "__main__":
     readFile("top-20-common-SSH-passwords.txt")
+
+h.hexdigest()
+print(h)
