0206ccf2df4900daf85722633329fe86cfea3994
ahrand <ahrand@vt.edu>
2024-12-04 14:45:47 -0500

    hexdigest stuff

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -1,7 +1,11 @@
 import hashlib
 from hashlib import blake2b
 h = blake2b()
+user2b = blake2b()
 
+userinput = input("Enter Password: ")
+print("Password is " + userinput)
+user2b.update(b'userinput')
 
 def readFile(inFile):
     file = open(inFile, "r")
@@ -9,10 +13,12 @@ def readFile(inFile):
     wordsList = words.split("\n")
     for word in wordsList:
         h.update(b'word')
-    print(wordsList)
+    #print(wordsList)
     
 if __name__ == "__main__":
     readFile("top-20-common-SSH-passwords.txt")
 
-h.hexdigest()
-print(h)
+hhexdigest = h.hexdigest()
+user2bhexdigest = user2b.hexdigest()
+print(hhexdigest)
+print(user2bhexdigest)
