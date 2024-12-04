bbb5610eb333d6f32c458c53a700f7ed47b3ff4d
ahrand <ahrand@vt.edu>
2024-12-04 14:52:17 -0500

    sha256 fixed

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -1,10 +1,10 @@
 import hashlib
 from hashlib import blake2b
-m = hashlib.sha256()
+userPassword = hashlib.sha256()
 
 userinput = input("Enter Password\nRestrictions - strictly composed of one to three words from the dictionary\nYour Password: ")
 print("Password is " + userinput)
-m.update(b'userinput')
+userPassword.update(b'userinput')
 
 def readFile(inFile):
     file = open(inFile, "r")
@@ -17,7 +17,5 @@ def readFile(inFile):
 if __name__ == "__main__":
     readFile("top-20-common-SSH-passwords.txt")
 
-hhexdigest = h.hexdigest()
-user2bhexdigest = user2b.hexdigest()
-print(hhexdigest)
-print(user2bhexdigest)
+userHexdigest = userPassword.hexdigest()
+print(userHexdigest)
