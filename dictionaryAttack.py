b4f8378a13600768c34b1cb827b284262e77d792
ahrand <ahrand@vt.edu>
2024-12-04 15:28:34 -0500

    incomplete for loop

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -17,11 +17,17 @@ if __name__ == "__main__":
     userPassword256.update(b'userinput')
     userPassword512.update(b'userinput')
     wordList = readFile("top-20-common-SSH-passwords.txt")
+    print(userPassword256)
+    print("\n\n")
+    for word in wordList:
+        word256 = hashlib.sha256()
+        word256.update(b'word')
+        print(word)
+        print(word256)
+        if(word256 == userPassword256):
+            print("\nFOUND\n")
 
     userHexdigest256 = userPassword256.hexdigest()
     print(userHexdigest256)
     userHexdigest512 = userPassword512.hexdigest()
     print(userHexdigest512)
-
-
-
