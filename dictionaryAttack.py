23579b2346904de764c348a41aa6eec3853b7bbe
ahrand <ahrand@vt.edu>
2024-12-04 15:14:18 -0500

    moved everything to main

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -1,11 +1,5 @@
 import hashlib
-userPassword256 = hashlib.sha256()
-userPassword512 = hashlib.sha512()
 
-userinput = input("Enter Password\nRestrictions - strictly composed of one to three words from the dictionary\nYour Password: ")
-print("Password is " + userinput)
-userPassword256.update(b'userinput')
-userPassword512.update(b'userinput')
 
 def readFile(inFile):
     file = open(inFile, "r")
@@ -15,9 +9,19 @@ def readFile(inFile):
     return wordsList
     
 if __name__ == "__main__":
+    userPassword256 = hashlib.sha256()
+    userPassword512 = hashlib.sha512()
+
+    userinput = input("Enter Password\nRestrictions - strictly composed of one to three words from the dictionary\nYour Password: ")
+    print("Password is " + userinput)
+    userPassword256.update(b'userinput')
+    userPassword512.update(b'userinput')
     wordList = readFile("top-20-common-SSH-passwords.txt")
 
-userHexdigest256 = userPassword256.hexdigest()
-print(userHexdigest256)
-userHexdigest512 = userPassword512.hexdigest()
-print(userHexdigest512)
+    userHexdigest256 = userPassword256.hexdigest()
+    print(userHexdigest256)
+    userHexdigest512 = userPassword512.hexdigest()
+    print(userHexdigest512)
+
+
+
