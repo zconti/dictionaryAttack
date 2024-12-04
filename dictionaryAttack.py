d2ad61b377eabb5fef0cbd5e8526cd450091d959
ahrand <ahrand@vt.edu>
2024-12-04 14:59:28 -0500

    removed unneccessary hash from readFile

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -11,9 +11,7 @@ def readFile(inFile):
     file = open(inFile, "r")
     words = file.read()
     wordsList = words.split("\n")
-    for word in wordsList:
-        h.update(b'word')
-    #print(wordsList)
+    print(wordsList)
     
 if __name__ == "__main__":
     readFile("top-20-common-SSH-passwords.txt")
