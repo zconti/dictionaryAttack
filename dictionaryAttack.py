d4eb329a5dc616ef5d2cbf6de5660dbe3b1fa154
ahrand <ahrand@vt.edu>
2024-12-09 15:00:09 -0500

    added 512

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -10,16 +10,30 @@ def readFile(inFile):
     return wordsList
     
 def dictionary256(userPassword256, wordList):
-    start_time = time.perf_counter()
+    start_time256 = time.perf_counter()
     for word in wordList:
         wordHexdigest256 = hashlib.sha256(word.encode('utf-8')).hexdigest()
         print(wordHexdigest256)
-        if(wordHexdigest256 == userPassword256):
+        if wordHexdigest256 == userPassword256:
             print("FOUND\n")
-    end_time = time.perf_counter()
+            break
+    end_time256 = time.perf_counter()
     print("Done?")
-    elapsed_time = end_time - start_time
-    return elapsed_time
+    elapsed_time256 = end_time256 - start_time256
+    return elapsed_time256
+
+def dictionary512(userPassword512, wordList):
+    start_time512 = time.perf_counter()
+    for word in wordList:
+        wordHexdigest512 = hashlib.sha256(word.encode('utf-8')).hexdigest()
+        print(wordHexdigest512)
+        if wordHexdigest512 == userPassword512:
+            print("FOUND\n")
+            break
+    end_time512=  time.perf_counter()
+    print("Done?")
+    elapsed_time512 = end_time512 - start_time512
+    return elapsed_time512
 
 if __name__ == "__main__":
     userPassword256 = hashlib.sha256()
@@ -35,10 +49,10 @@ if __name__ == "__main__":
     print(userHexdigest256)
     print("\n\n")
 
-    time256 = dictionary256(userPassword256, wordList)
-
+    time256 = dictionary256(userHexdigest256, wordList)
+    time512 = dictionary256(userHexdigest512, wordList)
+    print(time256)
+    print(time512)
     
-    userHexdigest256 = userPassword256.hexdigest()
     print(userHexdigest256)
-    userHexdigest512 = userPassword512.hexdigest()
     print(userHexdigest512)
