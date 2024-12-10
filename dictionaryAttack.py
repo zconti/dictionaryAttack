d99f78376ddb0d20b21873fd9fa86fd4aac33ced
zconti <zconti@vt.edu>
2024-12-09 23:00:20 -0500

    final

--- dictionaryAttack.py
+++ dictionaryAttack.py
@@ -6,7 +6,6 @@ def readFile(inFile):
     file = open(inFile, "r")
     words = file.read()
     wordsList = words.split("\n")
-    print(wordsList)
     return wordsList
     
 def dictionary256(userPassword256, wordList):
@@ -19,9 +18,7 @@ def dictionary256(userPassword256, wordList):
             for word in wordList:
                 counter += 1
                 comboWord = firstWord + secondWord + word
-                #print(comboWord)
                 wordHexdigest256 = hashlib.sha256(comboWord.encode('utf-8')).hexdigest()
-                #print(wordHexdigest256)
                 if wordHexdigest256 == userPassword256:
                     print("FOUND\n")
                     end_time256 = time.perf_counter()
@@ -39,9 +36,7 @@ def dictionary512(userPassword512, wordList):
             for word in wordList:
                 counter += 1
                 comboWord = firstWord + secondWord + word
-                #print(comboWord)
                 wordHexdigest512 = hashlib.sha512(comboWord.encode('utf-8')).hexdigest()
-                #print(wordHexdigest256)
                 if wordHexdigest512 == userPassword512:
                     print("FOUND\n")
                     end_time512 = time.perf_counter()
@@ -50,43 +45,41 @@ def dictionary512(userPassword512, wordList):
     return 0
 
 if __name__ == "__main__":
-    userPassword256 = hashlib.sha256()
-    userPassword512 = hashlib.sha512()
-
     userinput = input("Enter Password\nRestrictions - strictly composed of one to three words from the dictionary\nYour Password: ")
-    print("Password is " + userinput)
-    userPassword256.update(userinput.encode('utf-8'))
-    userHexdigest256 = userPassword256.hexdigest()
-    userPassword512.update(userinput.encode('utf-8'))
-    userHexdigest512 = userPassword512.hexdigest()
-    wordList = readFile("200-worst-passwords.txt")
-    print(userHexdigest256)
-    print(userHexdigest512)
-    print("\n\n")
+    sha256times = []
+    sha512times = []
+    names = []
+    guesses = []
+    count = 0
+    while(userinput != "q"):
+        count += 1
+        userPassword256 = hashlib.sha256()
+        userPassword512 = hashlib.sha512()
 
-    time256 = dictionary256(userHexdigest256, wordList)
-    time512 = dictionary512(userHexdigest512, wordList)
-    print(time256[0])
-    print(time512[0])
-    print(time256[1])
-    print(time512[1])
-    
-    print(userHexdigest256)
-    print(userHexdigest512)
-    
-    
-    names = ['SHA256', 'SHA512']
-    times = [time256[0], time512[0]]
-    guesses = [time256[1], time512[1]]
-    words = len(wordList)-1
-    
+        print("Password is " + userinput)
+        userPassword256.update(userinput.encode('utf-8'))
+        userHexdigest256 = userPassword256.hexdigest()
+        userPassword512.update(userinput.encode('utf-8'))
+        userHexdigest512 = userPassword512.hexdigest()
+        wordList = readFile("200-worst-passwords.txt")
 
-    plt.figure(figsize=(9, 3))
+        time256 = dictionary256(userHexdigest256, wordList)
+        time512 = dictionary512(userHexdigest512, wordList)
+    
+    
+        names.append((str)(count))
+        sha256times.append(time256[0])
+        sha512times.append(time512[0])
+        guesses.append(time256[1])
+        words = len(wordList)-1
+    
+        userinput = input("Enter Password\nRestrictions - strictly composed of one to three words from the dictionary\nYour Password: ")
+    plt.figure(figsize=(12,4))
 
     plt.subplot(131)
-    plt.bar(names, times)
+    plt.bar(names, sha512times)
     plt.ylabel('Time(sec)')
-    plt.title('Time Taken to Crack')
+    plt.title('Time Taken to Crack SHA512')
     plt.subplot(132)
     plt.bar(names, guesses)
     plt.ylabel('Guesses')
@@ -95,4 +88,21 @@ if __name__ == "__main__":
     plt.bar("Dictionary", words)
     plt.ylabel('Words')
     plt.title('Words in Dictionary')
-    plt.savefig('plot.png')
+    plt.tight_layout()
+    plt.savefig('512stats.png')
+    
+    plt.figure(figsize=(12,4))
+    plt.subplot(131)
+    plt.bar(names, sha256times)
+    plt.ylabel('Time(sec)')
+    plt.title('Time Taken to Crack SHA256')
+    plt.subplot(132)
+    plt.bar(names, guesses)
+    plt.ylabel('Guesses')
+    plt.title('Number of Guesses')
+    plt.subplot(133)
+    plt.bar("Dictionary", words)
+    plt.ylabel('Words')
+    plt.title('Words in Dictionary')
+    plt.tight_layout()
+    plt.savefig('256stats.png')
